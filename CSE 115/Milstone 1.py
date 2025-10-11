48c1a85c36e04caf5108df7b11c18d0949dee425
Afeefur Rahman <afeefurrahman2007@gmail.com>
2025-10-11 18:09:45 -0400

    Updated Milstone 1.py

--- CSE 115/Milstone 1.py
+++ CSE 115/Milstone 1.py
@@ -11,4 +11,23 @@ def clampValueToRange(value,low,high):
         high_result = int(high)
         return high_result
     
-#def returnColor(line):
+def returnColor(line):
+    result_list = []
+    newString = line.replace(" ","")
+    for x in range(0,3):
+        result_list.append(newString[x])
+    return result_list
+
+def returnLocation(line):
+    resultList = []
+    newString = line.replace(" ", "")
+    for x in range(len(newString)-1,len(newString)-3,-1):
+        resultList.append(newString[x])
+        resultList.reverse()
+    return resultList
+
+def locationValid(x,width,y,height):
+    if x >= 0 and y >= 0 and x < width and y < height:
+        return True
+    else:
+        return False
